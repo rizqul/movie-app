4c8165e4d618c16059e1a4fd3d16fb18f1f6886d
rizqul <kikiqullah@gmail.com>
2021-06-10 23:11:25 +0800

    Initial commit

---
+++ app/src/main/java/com/resql/movie_app_final/utils/ActionBarTitle.java
@@ -0,0 +1,5 @@
+package com.resql.movie_app_final.utils;
+
+public interface ActionBarTitle {
+    void setActionBarTitle(String title);
+}
---
+++ app/src/main/java/com/resql/movie_app_final/utils/OnItemClick.java
@@ -0,0 +1,5 @@
+package com.resql.movie_app_final.utils;
+
+public interface OnItemClick{
+    void onClick(int pos);
+}
